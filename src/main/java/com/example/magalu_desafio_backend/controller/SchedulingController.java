0a9f6d4ee97f49d15c6a3841f5c27a341f6be6c6
Diego Nascimento <nascimentodln22@gmail.com>
2025-08-04 14:00:50 -0300

    adding get by id in controller

--- src/main/java/com/example/magalu_desafio_backend/controller/SchedulingController.java
+++ src/main/java/com/example/magalu_desafio_backend/controller/SchedulingController.java
@@ -5,10 +5,7 @@ import com.example.magalu_desafio_backend.business.dto.SchedulingOutDTO;
 import com.example.magalu_desafio_backend.business.service.SchedulingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +20,11 @@ public class SchedulingController {
 
         return ResponseEntity.ok(schedulingService.newCommunicationSchedule(schedulingDTO));
 
+    }
+
+    @GetMapping
+    public ResponseEntity<SchedulingOutDTO> getCommunicationScheduleById(@RequestParam("id") Long id){
+        return ResponseEntity.ok(schedulingService.getCommunicationSchedules(id));
     }
 
 }
