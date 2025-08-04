a3090371f69e1c4b23967ef039608ef604be50af
Diego Nascimento <nascimentodln22@gmail.com>
2025-08-04 11:33:36 -0300

    adding scheduling repository

---
+++ src/main/java/com/example/magalu_desafio_backend/infra/repository/SchedulingRepository.java
@@ -0,0 +1,9 @@
+package com.example.magalu_desafio_backend.infra.repository;
+
+import com.example.magalu_desafio_backend.infra.entity.Scheduling;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchedulingRepository extends JpaRepository<Scheduling, Long> {
+}
