b2103ece6630d76e82c76cbd689fa3d72dc5b218
Diego Nascimento <nascimentodln22@gmail.com>
2025-08-04 10:59:59 -0300

    first commit

---
+++ src/main/java/com/example/magalu_desafio_backend/MagaluDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.magalu_desafio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluDesafioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/magalu_desafio_backend/MagaluDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magalu_desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
